dff5fc5713b7e183f662e4e6400344a7a7d6d94c
Mathias Brams <Mathias@brams.dk>
2021-01-31 09:23:52 +0100

    ok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
         Mathias' website
       </header>
     </div>
